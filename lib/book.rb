f6d773945ef5e7831a06def2a1123a24c9dc5240
MAJAMES071 <majames071@gmail.com>
2020-11-23 22:56:23 -0700

    commit tree 3201126e205d9f1ed8e5aadf15392a359ffdab8f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+
+def initialize(title, author_name, page_count, genre)
+    @title = title
+    @author_name = author_name
+    @page_count = page_count
+    @genre = Genre
+end
+
+
+end
 
 
